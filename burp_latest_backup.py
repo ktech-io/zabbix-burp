fa8a175e25c7c86e86610b9df17b73fa9112c5ca
Patrick Brideau <pbrideau@kronostechnologies.com>
2016-10-12 16:34:40 -0400

    Lint python script

--- burp_latest_backup.py
+++ burp_latest_backup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python2
+"""Get the epoch format of the latest burp backup.
+
+This module will parse output of the burp cli and return the output
+"""
 import json
 import sys
 import re
@@ -8,92 +12,109 @@ import socket
 import os
 import fcntl
 
-def read_process(process,eol_string):
-    buffer = ""
+def read_process(process, eol_string):
+    """Read the process until there is nothig to read anymore"""
+    output = ""
     line = ""
     fcntl.fcntl(process.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
     while True:
         try:
             line = process.stdout.readline()
-            buffer += line
+            output += line
             if eol_string == line:
                 break
-        except IOError as e:
+        except IOError:
             if process.poll() != 0 and process.poll() != None:
                 break
-            pass
-    return buffer
+    return output
 
 def read_cache():
+    """Check the latest cached data"""
     try:
-        f = open('/var/tmp/burp_latest_backup_cache','r')
-        return f.read()
-        f.close()
-    except:
-        return '0'
+        cache_file = open('/var/tmp/burp_latest_backup_cache', 'r')
+        cached_string = cache_file.read()
+        cache_file.close()
+    except IOError:
+        cached_string = '0'
+
+    return cached_string
 
 def write_cache(timestamp):
-    #  When polling during a backup, the binary cannot connect.
-    #  It does not means there is no backup available.  So use a cache
-    f = open('/var/tmp/burp_latest_backup_cache','w')
-    f.write(timestamp)
-    f.close()
-BURP_REGEX_TIMESTAMP = '([0-9]{7}) ([0-9]{4}-(?:0[1-9]|1[012])-(?:0[1-9]|[12][0-9]|3[01])) ((?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])'
+    """Write timestamp to cache
+
+    When polling during a backup, the binary cannot connect.
+    It does not means there is no backup available.  So use a cache
+    """
+    cache_file = open('/var/tmp/burp_latest_backup_cache', 'w')
+    cache_file.write(timestamp)
+    cache_file.close()
 
 def get_burp1_latest_timestamp():
-    json = get_burp1_json()
-    latest_timestamp = parse_burp1_json(json)
+    """Main function for Burp version 1"""
+    json_output = get_burp1_json()
+    latest_timestamp = parse_burp1_json(json_output)
     return latest_timestamp
 
 def get_burp1_json():
+    """Read output of the command"""
     burp_command = ("/usr/sbin/burp", "-a", "list", "-j")
-    burp_json_output = subprocess.Popen(burp_command,stdout=subprocess.PIPE).communicate()[0]
+    burp_json_output = subprocess.Popen(burp_command, stdout=subprocess.PIPE).communicate()[0]
     data = json.loads(burp_json_output)
 
     return data
 
 def parse_burp1_json(json_object):
-    BURP_REGEX_TIMESTAMP = '([0-9]{7}) ([0-9]{4}-(?:0[1-9]|1[012])-(?:0[1-9]|[12][0-9]|3[01])) ((?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])'
- 
+    """Parse the json"""
+    burp_regex_timestamp = ('([0-9]{7}) '
+                            '([0-9]{4}-(?:0[1-9]|1[012])-(?:0[1-9]|[12][0-9]|3[01])) '
+                            '((?:[01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9])'
+                           )
+
     latest_burp_timestamp = json_object['backups'][-1]['timestamp']
- 
-    p = re.compile(BURP_REGEX_TIMESTAMP) # parse the date yyyy-mm-dd
-    m = p.search(latest_burp_timestamp)
-    latest_burp_datetime = datetime.datetime.strptime(m.group(2) + ' ' + m.group(3), '%Y-%m-%d %H:%M:%S') # return the datetime part of the burp timestamp
+
+    compiled_regex = re.compile(burp_regex_timestamp) # parse the date yyyy-mm-dd
+    regex_result = compiled_regex.search(latest_burp_timestamp)
+    # return the datetime part of the burp timestamp
+    latest_burp_datetime = datetime.datetime.strptime(
+        regex_result.group(2) + ' ' + regex_result.group(3),
+        '%Y-%m-%d %H:%M:%S'
+    )
     latest_burp_datetime_epoch = latest_burp_datetime.strftime('%s')
 
     return latest_burp_datetime_epoch
- 
+
 def get_burp2_latest_timestamp():
-    json = get_burp2_json()
-    latest_timestamp = float(parse_burp2_json(json))
+    """Main function for Burp version 2"""
+    json_output = get_burp2_json()
+    latest_timestamp = float(parse_burp2_json(json_output))
     # The burp monitor does not return the epoch format in UTC, but in server localtime.
     # So convert it so we output it in UTC
     latest_timestamp_utc = datetime.datetime.utcfromtimestamp(latest_timestamp).strftime('%s')
     return latest_timestamp_utc
-        
+
 def get_burp2_json():
-    burp_command = ("/usr/sbin/burp","-a","monitor")
+    """Read output of the command"""
+    burp_command = ("/usr/sbin/burp", "-a", "monitor")
 
     # Make sure output is clean
     process = subprocess.Popen(burp_command, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-    read_process(process,"{ \"logline\": \"in monitor\" }\n")
+    read_process(process, "{ \"logline\": \"in monitor\" }\n")
 
     fqdn = socket.getfqdn()
-    burp_request = "c:{fqdn}\n".format(**locals())
+    burp_request = "c:{0}\n".format(fqdn)
     process.stdin.write(burp_request)
     process.stdin.flush()
-    burp_json_output = read_process(process,"}\n")
+    burp_json_output = read_process(process, "}\n")
     process.terminate()
     try:
         data = json.loads(burp_json_output)
     except ValueError:
         data = json.loads('[]')
-        pass
 
     return data
 
 def parse_burp2_json(json_object):
+    """Parse the json"""
     try:
         for backup in json_object['clients'][0]['backups']:
             if 'current' in backup['flags']:
@@ -106,7 +127,8 @@ def parse_burp2_json(json_object):
     return latest_backup
 
 def get_burp_version():
-    burp_command = ("/usr/sbin/burp","-v")
+    """Get the burp version"""
+    burp_command = ("/usr/sbin/burp", "-v")
     process = subprocess.Popen(burp_command, stdout=subprocess.PIPE)
     burp_version = process.communicate()[0]
 
@@ -114,20 +136,22 @@ def get_burp_version():
         return 1
     elif 'burp-2' in burp_version:
         return 2
-    raise
- 
-if __name__ == "__main__":
- 
+    raise "Unknown burp version '{0}'".format(burp_version)
+
+def main():
+    """Main function"""
     burp_version = get_burp_version()
-    
+
     try:
         if burp_version == 1:
             timestamp = get_burp1_latest_timestamp()
         elif burp_version == 2:
             timestamp = get_burp2_latest_timestamp()
         print timestamp
-    except:
+    except BaseException:
         sys.stderr.write("Unexpected error: {0}".format(sys.exc_info()[0]))
-        raise
         sys.exit(1)
+
+if __name__ == "__main__":
+    main()
 
